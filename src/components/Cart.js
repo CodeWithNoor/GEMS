7d47a9e139bd57c4f37bf8f7d26d2808a6bba298
Noor <ma8073641@gmail.com>
2022-11-02 12:16:19 -0700

    cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Cart = () => {
   return (
     <div>
-      
+      <p>Cart</p>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
